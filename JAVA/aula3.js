f9e0c26db9732a33c2409798ec5253f809d48da0
MatheusGuilhermeRibeiro <matheusguilhermeribeirodev@gmail.com>
2023-09-21 15:00:57 -0300

    Aula 2

---
+++ JAVA/aula3.js
@@ -0,0 +1,17 @@
+// const usuarioLogado = true
+// const contapaga = false
+//
+// console.log(0 == false)
+// console.log("" == false)
+// console.log(1 == true)
+
+
+// null = vazio ou nada
+let minhaVar;
+let varnull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varnull)
