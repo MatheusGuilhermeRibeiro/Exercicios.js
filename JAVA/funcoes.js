859f8f7f2a0021bdfd6df8c0309abe948adb5766
MatheusGuilhermeRibeiro <matheusguilhermeribeirodev@gmail.com>
2023-09-25 09:38:01 -0300

    operadores e funções

---
+++ JAVA/funcoes.js
@@ -0,0 +1,15 @@
+// let x = "";
+// console.log(x)
+// x = "oi";
+
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+//teste Math.round arrendondando de 5.8 para 6 
+imprimeTexto(Math.round(soma()));
+// imprimeTexto("outro texto");
+
+function soma(){
+    // return smp a ultima linha do bloco
+    return  3.1 + 2.7;
+}
---
+++ JAVA/template-strings.js
@@ -0,0 +1,11 @@
+const nome = "Ju";
+const idade = 2023 - 1981;
+const cidadeNascimento = "São paulo";
+
+// const apre = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci em " + cidadeNascimento;
+
+const apres = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+
+console.log(apres)
+
+
