cb20ab4e3d9d9671101f618a56783632a573582c
MatheusGuilhermeRibeiro <144956645+MatheusGuilhermeRibeiro@users.noreply.github.com>
2023-09-28 14:36:21 -0300

    Update EX31.js

--- EX31.js
+++ EX31.js
@@ -6,10 +6,19 @@ console.log("Como jogar: Informe [1] PEDRA [2] PAPEL [3] TESOURA");
 console.log("---------------------------------------------------");
 const prompt = require('prompt');
 prompt.start();
-prompt.get(["jogada1"], function (err, result) {
-   const jogada1 = parseInt(result.jogada1);
-   const jogada2 = parseInt(result.jogada2);
-   if (jogada == 1) {
-      
+prompt.get(["jogador1","jogador2"], function (err, result) {
+   const jogador1 = parseInt(result.jogador1);
+   const jogador2 = parseInt(result.jogador2);
+   if (jogador1 == 1 && jogador2 == 3 ||jogador1 == 2 && jogador2 == 1 || jogador1 == 3 && jogador2 == 2) {
+      console.log("Jogador 1 venceu");
+   } else if (jogador2 == 1 && jogador1 == 3 ||jogador2 == 2 && jogador1 == 1 || jogador2 == 3 && jogador1 == 2) {
+      console.log("Jogador 2 venceu");
+   } else if (jogador1 > 3 || jogador2 > 3){
+      console.log("Jogada INVALIDA");
+   }else{
+      console.log("EMPATE");
    }
+// 1 > 3
+// 2 > 1
+// 3 > 2
 });
